7860a4461d0807f80913242cdf955e1cd7c45129
IMGEONWOO <hg01040@naver.com>
2024-05-07 04:46:49 +0900

    Add files via upload

--- Ex.java
+++ Ex.java
@@ -1,14 +1,11 @@
 public class Ex {
     public static void main(String[] args) {
-        Static s1, s2;
-        s1 = new Static();
-        s1.n = 5;
-        s1.g();
-        s1.m = 50;
-        s2 = new Static();
-        s2.n = 8;
-        s2.h();
-        s2.f();
+        StaticSample.m = 10;
+
+        StaticSample  s1;
+        s1 = new StaticSample();
         System.out.println(s1.m);
+        s1.f();
+        StaticSample.f();
     }
 }
